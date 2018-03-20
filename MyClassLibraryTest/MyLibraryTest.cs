a12af250c903278661166d4e3f5f008baefc12c9
Michael <michael.seldin@microfocus.com>
2018-03-20 15:22:54 +0200

    initial commit

---
+++ MyClassLibraryTest/MyLibraryTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyClassLibrary;
+
+namespace MyClassLibraryTest
+{
+    [TestClass]
+    public class MyLibraryTest
+    {
+        [TestMethod]
+        public void TestMathPass()
+        {
+            var myLibrary = new MyLibrary();
+
+            var res = myLibrary.Plus(1, 1);
+
+            Assert.AreEqual(2, res);
+        }
+
+        [TestMethod]
+        public void TestMathFail()
+        {
+            var myLibrary = new MyLibrary();
+
+            var res = myLibrary.Plus(1, 1);
+            Assert.AreNotEqual(2, res);
+        }
+    }
+}
